b00789f6ad512da5d07827be316b29833080fc0b
fizlip <fillatino@gmail.com>
2025-06-27 09:55:21 +0200

    tidy: remove test auth token

--- app/sse/message/route.ts
+++ app/sse/message/route.ts
@@ -15,10 +15,6 @@ export async function POST(request: Request) {
       method: 'POST',
       body: request.body,
       duplex: 'half', // Required when forwarding a request body
-      // headers: {
-      //   'Content-Type': 'application/json',
-      //   'Authorization': "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6InNzb19vaWRjX2tleV9wYWlyXzAxSlRNMFBGNE02RU1ZMFJHUVZNTk5YUUhEIn0.eyJpc3MiOiJodHRwczovL3NpZ25pbi5tY3Auc2hvcCIsImF1ZCI6ImNsaWVudF8wMUpUTTBQRjZKVFdGMFA1NjBYRVpWRVE2QSIsInN1YiI6InVzZXJfMDFKWTRCODI0NVlBTU1YR1JHUTM3N1pCOEQiLCJzaWQiOiJhcHBfY29uc2VudF8wMUpZNlkwRzI0WUgzRzM5Q1RXQUo4TTBQMiIsImp0aSI6IjAxSlk2WTBIUzhGMzYxNkVEQ1o1RFRIVkU0IiwiZXhwIjoxNzUwNDM1NTc1LCJpYXQiOjE3NTA0MzE5NzV9.ExlZES7-NNqY0ah7i73AnUTFMwUcxv8UuQa2Tjav0Zel7Cz6ccpjqmL74N4AyF3uW_gQsv2yH7wPwNYBPAG4EDicsirnawkw0vU8WJoXimOlCtVA-kkhQDdon4LGlSFDc4lhE03a1kPHSuhBRRrC3NWOl4ohqMo443B6pHkJ852Zok5h8m8y78f94wOEpyxekbYzn6xC_rM2_dFjqHnMXfUt67-GSF4-nMHaRT-pcSlRjkjyaQgAj5b89-8WFkzy74ynnd1VI0fg9vANdqC15q1AQGanz_fHxKNcNe1CjRztpBQnqDutZY5O-Mx24bUfL92gScnJEQ0hOcuMr7Sjrw"
-      // },
     };
     
     const response = await fetch("" + session_id, fetchOptions);
